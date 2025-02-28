abc07d128ee4085f27f77d95d3b6999d715149b5
samiksha-jain1310 <samikshasidharth1307@gmail.com>
2025-02-28 17:41:41 +0530

    Add files via upload

---
+++ DAY1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                /*
+                Console.WriteLine("Enter the length of rectangle: ");
+                int length = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter the width of rectangle: ");
+                int width = Convert.ToInt32(Console.ReadLine());
+                int area = length * width;
+                
+                Console.WriteLine($"The area of rectangle is: {area}");*/
+
+            Console.WriteLine("Enter 1st number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter 2nd number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int c = a + b;
+            Console.WriteLine($"The Addition of two numbers is: {c}");
+        }
+    }
+}
